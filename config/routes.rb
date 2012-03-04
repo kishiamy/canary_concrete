fe32c8ce0bdf5a2fac93d2d2cb09153717d4b51e
Juan Fraire <groteckgmail.com>
2012-03-04 23:09:15 +0000

    mistake solved

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ CanaryConcrete::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => '/users/sign_in'
+   root :to => 'Users#sign_in'
 
   # See how all your routes lay out with "rake routes"
 
