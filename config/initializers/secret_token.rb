c119d2dc692d40ee428d06643e0d43c3faa2d71d
Juan Fraire <groteckgmail.com>
2012-03-04 16:33:10 +0000

    mi primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanaryConcrete::Application.config.secret_token = 'd5a93577fa3a050f5b67397418ae624f05fb8802a262e075e820227306d32901fefb77c367d9d0b5670f8365410c26ea88308c9a19349749eae37cc6d68e0263'
