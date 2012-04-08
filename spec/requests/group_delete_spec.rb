e881cb4ef4c71b82dd8abf90fb9cb0b28191474a
Juan Fraire <groteck@gmail.com>
2012-04-08 02:26:30 +0100

    fixed delete test

--- spec/requests/group_delete_spec.rb
+++ spec/requests/group_delete_spec.rb
@@ -3,13 +3,15 @@ require 'spec_helper'
 describe "delete group" do
     before do
       @admin = Factory(:admin)
-      @group= Factory(:group, name: "Delete_me")
+      @group = Factory(:group, name: "Delete_me")
       login @admin
+      @count = Group.count
       visit groups_path
       find(".btn-danger[href='#{group_path(@group)}']").click
       page.driver.browser.switch_to.alert.accept
     end
     it "group was deleted successfully" do
-      Group.count.should == 1
+      current_path.should == groups_path
+      Group.count.should be_eql (@count - 1)  
     end
 end
