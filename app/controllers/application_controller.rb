af79d5aec783ed0282497bba2882767171d73e38
Ayose <ayosec@gmail.com>
2012-05-13 12:55:22 +0100

    Move #auto_pages to PagesHelper
    The #auto_pages has be defined in the PagesHelper. It makes no sense to
    be defined in the ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,19 +13,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :is_admin
 
-
-  def auto_pages
-    html = ""
-    Page.tree.each_key do |key|
-      html << "<li class='plegate li_menu'>"
-      html << "<a href='/pages/" + Page.tree[key].id.to_s + "'>" + Page.tree[key].title + "</a>"
-      html << recursive_childs(Page.tree[key]["hijos"].rehash)
-      html << "</li>"
-    end
-    return html.html_safe
-  end
-  helper_method :auto_pages
-
   def recursive_childs newtree={}
     newhtml = "<ul>"
     newtree.each_key do |key|
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+
+  def auto_pages
+    html = ""
+    Page.tree.each_key do |key|
+      html << "<li class='plegate li_menu'>"
+      html << "<a href='/pages/" + Page.tree[key].id.to_s + "'>" + Page.tree[key].title + "</a>"
+      html << recursive_childs(Page.tree[key]["hijos"].rehash)
+      html << "</li>"
+    end
+    return html.html_safe
+  end
+
 end
