988b9c7e15a7087d3de76b1728fcb1fa97f86a6c
Ayose <ayosec@gmail.com>
2012-05-13 13:34:18 +0100

    Rewrite the page tree
    * Use `content_tag`. A cleaner and safer way to generate HTML.
    * Remove a lot of useless code.
    * Vastly simplified version of the pages tree generator

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,25 +1,13 @@
 module PagesHelper
 
-  def recursive_childs newtree={}
-    newhtml = "<ul>"
-    newtree.each_key do |key|
-      newhtml << "<li class='plegate'>"
-      newhtml << "<a href='/pages/" + newtree[key].id.to_s + "'>" + newtree[key].title + "</a>"  
-      newhtml << recursive_childs(newtree[key]["hijos"].rehash)
-      newhtml << "</li>"
+  def pages_tree(pages = Page.roots)
+    content_tag :ul do
+      pages.map do |page|
+        content_tag :li, class: "plegate" do
+          link_to(page.title, page_path(page)) + pages_tree(page.pages)
+        end
+      end.join.html_safe
     end
-    return newhtml << "</ul>"
-  end   
-
-  def auto_pages
-    html = ""
-    Page.tree.each_key do |key|
-      html << "<li class='plegate li_menu'>"
-      html << "<a href='/pages/" + Page.tree[key].id.to_s + "'>" + Page.tree[key].title + "</a>"
-      html << recursive_childs(Page.tree[key]["hijos"].rehash)
-      html << "</li>"
-    end
-    return html.html_safe
   end
 
 end
