b66188fff7c6d05f914c77794f16a5656be9c0a4
kishiamy <kishiamy84@gmail.com>
2012-06-13 11:33:15 +0100

    ....dary, legendary

---
+++ app/controllers/error_controller.rb
@@ -0,0 +1,7 @@
+class ErrorController < ApplicationController
+  skip_before_filter :set_locale
+
+  def error_404
+    render status: 404, layout: nil 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,66 +9,13 @@ CanaryConcrete::Application.routes.draw do
     match "admin/groups/:id/edit" => "admin#update", :via => :put, :as => :edit_user_group
 
     root :to => 'pages#index'
-    match "*path", to: "locale#not_found"
-  end 
+    match '*path', to: redirect("/#{I18n.default_locale}/%{path}"), :constraints => lambda {|req|
+	I18n.default_locale != req.path.split('/')[1].to_sym
+    }
+    
+    match '*path', to: 'error#error_404'
+  end
   match '*path', to: redirect("/#{I18n.default_locale}/%{path}")
   match '', to: redirect("/#{I18n.default_locale}/pages")
    
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-
-
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
