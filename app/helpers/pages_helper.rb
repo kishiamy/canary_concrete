4f25f61ebce44e13a911269cc36444d4a4acbdbe
groteck <groteck@sobremesa.(none)>
2012-06-28 04:01:51 +0100

    change > by &gt;

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,7 +2,7 @@ module PagesHelper
   def breadcrumb(page)
     pages = page.title
     if page.page
-      pages.insert(0, "#{parents(page.page)} > ")
+      pages.insert(0, "#{parents(page.page)} &gt; ")
     end
     return pages.html_safe
   end
@@ -10,7 +10,7 @@ module PagesHelper
   def parents(page)
     pages = link_to(page.title, page_path(page)) 
     if page.page
-      pages.insert(0, "#{parents(page.page)} > ")
+      pages.insert(0, "#{parents(page.page)} &gt; ")
     else
       return pages.html_safe
     end
