e2bd825a90c2459c59f0d379dcfd56a397eb71bf
kishiamy <kishiamy84@gmail.com>
2012-08-06 11:53:25 +0100

    adding validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :approved
   belongs_to :group
-
+  validates :approved, :presence => true
 end
