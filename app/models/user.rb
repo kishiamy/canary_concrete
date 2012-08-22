a35e0d706fc0cc3f7f3b9c42bd170face4658123
kishiamy <kishiamy84@gmail.com>
2012-08-23 00:48:37 +0100

    solved mistake in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   belongs_to :group
-  before_create :default_approved
-  def default_approved
-    self.approved = false
-    true
+  def active_for_authentication?
+    super && approved?
   end
 end
